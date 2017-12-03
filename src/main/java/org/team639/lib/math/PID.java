10b0ae3cdc4facdbd0b8a6caa5aaabffc4312c60
Jack Greenberg <theProgrammerJack@gmail.com>
2017-12-03 17:21:26 -0500

    Commented the PID class.

--- src/main/java/org/team639/lib/math/PID.java
+++ src/main/java/org/team639/lib/math/PID.java
@@ -14,6 +14,17 @@ public class PID {
     private double lastError;
     private double lastOutput;
 
+    /**
+     * Constructs a new PID loop.
+     * @param p The P constant
+     * @param i The I constant
+     * @param d The D constant
+     * @param min The absolute value minimum value that can be returned
+     * @param max The absolute value maximum value that can be returned
+     * @param rate The maximum increase that can occur in one cycle
+     * @param tolerance The tolerance around the destination value within which the PID loop should stop.
+     * @param iCap The cap for how big the I value can grow.
+     */
     public PID(double p, double i, double d, double min, double max, double rate, double tolerance, double iCap) {
         kP = p;
         kI = i;
@@ -29,12 +40,23 @@ public class PID {
         lastOutput = 0;
     }
 
+    /**
+     * Changes the PID constants.
+     * @param p The new P constant
+     * @param i The new I constant
+     * @param d The new D constant
+     */
     public void setPID(double p, double i, double d) {
         kP = p;
         kI = i;
         kD = d;
     }
 
+    /**
+     * Computes the output for a given error.
+     * @param error The error
+     * @return The output of the PID calculation
+     */
     public double compute(double error) {
         if (Math.abs(error) < tolerance) return 0;
 
